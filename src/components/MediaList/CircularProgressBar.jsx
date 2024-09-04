90212dd0ef539b456097bfaf95c171cb0b5cd4b1
3duyyy <duyss1902@gmail.com>
2024-09-04 15:48:53 +0700

    MovieList & Top Rated List (refactor MovieList)

---
+++ src/components/MediaList/CircularProgressBar.jsx
@@ -0,0 +1,51 @@
+// import React from 'react'
+import PropTypes from "prop-types";
+
+const CircularProgressBar = ({ percent, size = 3, strokeWidth = 0.25 }) => {
+  const radius = size / 2 - strokeWidth;
+  const p = 2 * Math.PI * radius;
+  return (
+    <div>
+      <svg width={`${size}vw`} height={`${size}vw`}>
+        <circle
+          r={`${radius}vw`}
+          cx={`${size / 2}vw`}
+          cy={`${size / 2}vw`}
+          stroke="white"
+          strokeWidth={`${strokeWidth}vw`}
+        />
+        <circle
+          r={`${radius}vw`}
+          cx={`${size / 2}vw`}
+          cy={`${size / 2}vw`}
+          fill="none"
+          stroke={percent >= 70 ? "green" : percent >= 50 ? "yellow" : "red"}
+          strokeWidth={`${strokeWidth}vw`}
+          strokeDasharray={`${p}vw`}
+          strokeDashoffset={`${p - (percent / 100) * p}vw`}
+          transform="rotate(-90)"
+          style={{ transformOrigin: "center" }}
+          strokeLinecap="round"
+        />
+        <text
+          x={`${size / 2}vw`}
+          y={`${size / 2}vw`}
+          fill="white"
+          fontSize={`1.2vw`}
+          alignmentBaseline="middle"
+          textAnchor="middle"
+        >
+          {percent}
+        </text>
+      </svg>
+    </div>
+  );
+};
+
+CircularProgressBar.propTypes = {
+  percent: PropTypes.any,
+  size: PropTypes.any,
+  strokeWidth: PropTypes.any,
+};
+
+export default CircularProgressBar;
